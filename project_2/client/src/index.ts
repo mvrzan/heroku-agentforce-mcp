b9a2394b39e2f60a86981c8ccc45c22d7fd9b62e
mvrzan <mvrzan@icloud.com>
2025-07-15 16:25:47 -0400

    initial project 2 client commit

---
+++ project_2/client/src/index.ts
@@ -0,0 +1,21 @@
+import "dotenv/config";
+import { MCPClient } from "./utils/MCPClient.js";
+
+async function main() {
+  if (process.argv.length < 3) {
+    console.log("Usage: node build/index.js <path_to_server_script>");
+    return;
+  }
+
+  const mcpClient = new MCPClient();
+
+  try {
+    await mcpClient.connectToServer(process.argv[2]);
+    await mcpClient.chatLoop();
+  } finally {
+    await mcpClient.cleanup();
+    process.exit(0);
+  }
+}
+
+main();
