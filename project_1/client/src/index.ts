8d33617918e2f6d40854c8e779aa0b67a043ada7
mvrzan <mvrzan@icloud.com>
2025-08-13 10:33:45 -0400

    enhance logging in MCPClient index for better usage visibility

--- project_1/client/src/index.ts
+++ project_1/client/src/index.ts
@@ -1,19 +1,23 @@
 import "dotenv/config";
 import { MCPClient } from "./utils/MCPClient.js";
+import { getCurrentTimestamp } from "./utils/loggingUtil.js";
 
 async function main() {
   if (process.argv.length < 3) {
-    console.log("Usage: node build/index.js <path_to_server_script>");
+    console.log(`${getCurrentTimestamp()} - 📝 MCPClient Index - Usage: node build/index.js <path_to_server_script>`);
     return;
   }
 
   const mcpClient = new MCPClient();
 
   try {
+    console.log(`${getCurrentTimestamp()} - ⏳ MCPClient Index - Instantiating an MCP client...`);
+
     await mcpClient.connectToServer(process.argv[2]);
     await mcpClient.chatLoop();
   } finally {
     await mcpClient.cleanup();
+
     process.exit(0);
   }
 }
