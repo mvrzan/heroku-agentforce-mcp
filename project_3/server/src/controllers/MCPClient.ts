a4bc380737e221506a939165f6982a6631dd2d61
mvrzan <mvrzan@icloud.com>
2025-08-19 14:59:30 -0400

    remove MCPClient controller as it is no longer needed

--- project_3/server/src/controllers/MCPClient.ts
+++
@@ -1,6 +0,0 @@
-import { getCurrentTimestamp } from "../utils/loggingUtil.js";
-import { Request, Response } from "express";
-
-const MCPClient = (req: Request, res: Response) => {};
-
-export default MCPClient;
