e1398265a1fa0405e461fb46c4e33db25cd324c6
mvrzan <mvrzan@icloud.com>
2025-08-19 14:58:44 -0400

    refactor MCPServerRoutes to use specific controllers for remote and local MCP requests

--- project_3/server/src/routes/routes.ts
+++ project_3/server/src/routes/routes.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import middleware from "../middleware/middleware.js";
-import MCPClient from "../controllers/MCPClient.js";
+import RemoteMCPClientController from "../controllers/RemoteMCPClientController.js";
+import LocalMCPServerController from "../controllers/LocalMCPServerController.js";
 
 const MCPServerRoutes = Router();
 
-MCPServerRoutes.get("/mcp", middleware, MCPClient);
+MCPServerRoutes.post("/mcp/remote", middleware, RemoteMCPClientController);
+MCPServerRoutes.post("/mcp/local", middleware, LocalMCPServerController);
 
 export default MCPServerRoutes;
