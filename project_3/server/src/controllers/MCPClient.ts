1e19a1be29f38007622bacad3328beb1cac08187
mvrzan <mvrzan@icloud.com>
2025-08-19 14:14:25 -0400

    add a simple MCPClient function

---
+++ project_3/server/src/controllers/MCPClient.ts
@@ -0,0 +1,6 @@
+import { getCurrentTimestamp } from "../utils/loggingUtil.js";
+import { Request, Response } from "express";
+
+const MCPClient = (req: Request, res: Response) => {};
+
+export default MCPClient;
