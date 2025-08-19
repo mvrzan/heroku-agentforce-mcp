ccbeef8e3372b86db803a9088cf17bf51ab117c3
mvrzan <mvrzan@icloud.com>
2025-08-19 14:14:31 -0400

    update routes

--- project_3/server/src/routes/routes.ts
+++ project_3/server/src/routes/routes.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import { SSEFunction, messages } from "../controllers/SSEFunction.js";
-import { HTTPFunction } from "../controllers/HTTPFunction.js";
 import middleware from "../middleware/middleware.js";
+import MCPClient from "../controllers/MCPClient.js";
 
 const MCPServerRoutes = Router();
 
-MCPServerRoutes.get("/sse", middleware, SSEFunction);
-MCPServerRoutes.post("/messages", middleware, messages);
-MCPServerRoutes.post("/http", middleware, HTTPFunction);
+MCPServerRoutes.get("/mcp", middleware, MCPClient);
 
 export default MCPServerRoutes;
