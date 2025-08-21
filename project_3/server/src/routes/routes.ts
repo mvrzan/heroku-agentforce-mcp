cfebde4d443740d5bb3833ae417cd070506423ae
mvrzan <mvrzan@icloud.com>
2025-08-21 14:48:53 -0400

    refactor US weather routes to use specific controller methods

--- project_3/server/src/routes/routes.ts
+++ project_3/server/src/routes/routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import middleware from "../middleware/middleware.js";
-import { USWeatherController } from "../controllers/USWeatherController.js";
+import { usaWeatherAlerts, usaWeatherForecast } from "../controllers/USWeatherController.js";
 import { CanadianWeatherController } from "../controllers/CanadianWeatherController.js";
 
 const MCPServerRoutes = Router();
 
-MCPServerRoutes.get("/api/us-weather/alerts", middleware, USWeatherController.getUSWeatherAlerts);
-MCPServerRoutes.get("/api/us-weather/forecast", middleware, USWeatherController.getUSWeatherForecast);
+MCPServerRoutes.get("/api/us-weather/alerts", middleware, usaWeatherAlerts);
+MCPServerRoutes.get("/api/us-weather/forecast", middleware, usaWeatherForecast);
 MCPServerRoutes.get("/api/canada-weather/current", middleware, CanadianWeatherController.getCurrentWeather);
 MCPServerRoutes.get("/api/canada-weather/forecast", middleware, CanadianWeatherController.getWeatherForecast);
 
